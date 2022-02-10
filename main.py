0301f4dfd18757d5695928e8109485b9b75c9e10
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-09 22:32:30 -0600

    Update main.py
    Complete at this point. Could add greater functionality later on.

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+from art import logo
 
 
 # Add
 def add(n1, n2):
     return n1 + n2
 
+
 # Subtract
 def subtract(n1, n2):
     return n1 - n2
+
 
 # Multiply
 def multiply(n1, n2):
     return n1 * n2
 
+
 # Divide
 def divide(n1, n2):
     return n1 / n2
+
 
 operations = {
     "+": add,
@@ -23,14 +28,27 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
-op_symbol = input("Pick and operation from the line above: ")
-num2 = int(input("What's the second number?: "))
 
-calculation = operations[op_symbol]
-answer = calculation(num1, num2)
+def calculator():
+    print(logo)
 
-print(f"{num1} {op_symbol} {num2} = {answer}")
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    answer = num1
 
+    keep_going = 'y'
+    while keep_going == 'y':
+        temp = answer
+        op_symbol = input("Pick an operation: ")
+        new_num = float(input("What's the next number?: "))
+        calculation = operations[op_symbol]
+        answer = calculation(temp, new_num)
+        print(f"{temp} {op_symbol} {new_num} = {answer}")
+        keep_going = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to begin a new calculation. ")
+
+        if keep_going == 'n':
+            calculator()
+
+
+calculator()
